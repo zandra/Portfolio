f00ca3828551a0f8f12be4ecf00a8f3aefcf8ab5
Alex Stevens <zandra630@gmail.com>
2020-03-15 16:38:44 -0500

    Updates

---
+++ precache-manifest.b93449ba3182cbc06a64943611619213.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a31579cd783492861c8a859aaa70b3",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "ddcc4c848f8590be2fb5",
+    "url": "/Portfolio/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "d04bfaaa265fe9627dd9",
+    "url": "/Portfolio/static/js/2.ff1f8cc6.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/Portfolio/static/js/2.ff1f8cc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddcc4c848f8590be2fb5",
+    "url": "/Portfolio/static/js/main.4d09fe87.chunk.js"
+  },
+  {
+    "revision": "677a3f181a518a3c7e2b",
+    "url": "/Portfolio/static/js/runtime-main.a87c1e37.js"
+  }
+]);
